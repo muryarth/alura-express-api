b5378ace4339689c2899d66acda578074b246d5e
Arthur Mury <arthur.mury@outlook.com>
2023-08-08 14:19:54 -0300

    .env change

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,12 +1,6 @@
 import mongoose from "mongoose";
+// import "dotenv/config";
 
-// define o caminho para o .env
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// dotenv.config({ path: path.resolve(__dirname, ".env") });
-
-// console.log(process.env.db_username, process.env.db_password);
 mongoose.connect(`mongodb+srv://${process.env.db_username}:${process.env.db_password}@cluster0.1zmcs.mongodb.net/alura-node`);
 
 let db = mongoose.connection;
